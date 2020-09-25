fcaee39f0258285b9b5b07202f08cff90108d6da
Dinos Kousidis <dinos@weave.works>
2020-09-25 15:45:13 +0200

    Remove https, http from domain in GetCloneURL

--- gitprovider/repositoryref_test.go
+++ gitprovider/repositoryref_test.go
@@ -490,6 +490,30 @@ func TestGetCloneURL(t *testing.T) {
 			want:      "ssh://git@my-gitlab.com:6443/luxas/test-org/other/foo-bar",
 		},
 		{
+			name:      "org: ssh",
+			repoinfo:  newOrgRepoRef("my-gitlab.com:6443", "luxas", []string{"test-org", "other"}, "foo-bar"),
+			transport: TransportTypeSSH,
+			want:      "ssh://git@my-gitlab.com:6443/luxas/test-org/other/foo-bar",
+		},
+		{
+			name:      "org: ssh",
+			repoinfo:  newOrgRepoRef("https://my-gitlab.com", "luxas", []string{"test-org", "other"}, "foo-bar"),
+			transport: TransportTypeSSH,
+			want:      "ssh://git@my-gitlab.com/luxas/test-org/other/foo-bar",
+		},
+		{
+			name:      "org: ssh",
+			repoinfo:  newOrgRepoRef("https://my-gitlab.com:6443", "luxas", []string{"test-org", "other"}, "foo-bar"),
+			transport: TransportTypeSSH,
+			want:      "ssh://git@my-gitlab.com:6443/luxas/test-org/other/foo-bar",
+		},
+		{
+			name:      "org: ssh",
+			repoinfo:  newOrgRepoRef("http://my-gitlab.com:6443", "luxas", []string{"test-org", "other"}, "foo-bar"),
+			transport: TransportTypeSSH,
+			want:      "ssh://git@my-gitlab.com:6443/luxas/test-org/other/foo-bar",
+		},
+		{
 			name:      "org: none",
 			repoinfo:  newOrgRepoRef("my-gitlab.com:6443", "luxas", []string{"test-org", "other"}, "foo-bar"),
 			transport: TransportType("random"),
